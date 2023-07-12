d0b5114267a71129d63d3d6cf3fcc118570aff33
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-13 02:00:54 +0300

    Custom methods2

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   # has_many :users, through: :likes
   # has_many :users, through: :comments
+
+  def post_counter_update
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.limit(5).oder(created_at: :desc)
+  end
 end
