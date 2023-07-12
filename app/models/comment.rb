fb6a9c0bf0237854f209199d4fdfc30fbb3c0d25
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-13 02:00:38 +0300

    Custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: :comments_counter
   belongs_to :author, class_name: 'User'
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :post, counter_cache: :likes_counter
   belongs_to :author, class_name: 'User'
+
+  def like_counter_update
+    post.update(likes_counter: post.likes.count)
+  end
 end
