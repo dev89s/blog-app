d8b6f8b4bbdba8d14d8a1a7b9c3d2ee285e8ba4b
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-19 22:55:30 +0300

    Add posts#index & posts#show views + _comment & _post partials + posts stylesheet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.where(author_id: params[:user_id])
+    @comments = Comment.where(author_id: params[:user_id])
     @user = User.where(id: params[:user_id])[0]
   end
 
   def show
     @user = User.where(id: params[:user_id])[0]
+    @comments = Comment.where(post_id: params[:id])
     @post = Post.includes(:author).where(author_id: params[:user_id], id: params[:id])[0]
   end
 end
