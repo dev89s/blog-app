c48ac9ecd9694e0e67e9e0e84cd1eae928f2a7c9
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-21 16:39:53 +0300

    Add create post funcionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,30 @@ class PostsController < ApplicationController
     @post = Post.includes(:author).where(author_id: params[:user_id], id: params[:id])[0]
     @comments = Comment.where(post_id: params[:id])
   end
+
+  def new
+    post = Post.new
+    user = current_user
+    respond_to do |format|
+      format.html { render :new, locals: { post: post, user: user } }
+    end
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :text))
+    post.likes_counter = 0
+    post.comments_counter = 0
+    post.author_id = current_user.id
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = "Post is saved"
+          redirect_to "/users/#{current_user.id}/posts/"
+        else
+          flash.now[:error] = "Error: Post didn't save"
+          render :new, locals: { post: post, user: current_user }
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/users/:user_id/posts", to: "posts#index"
+  get "/users/:user_id/posts", to: "posts#index", as: 'posts'
+  get "/users/:user_id/posts/new", to: "posts#new", as: 'new_post'
+  post "/users/:user_id/posts/create", to: "posts#create"
   get "/users/:user_id/posts/:id", to: "posts#show"
   get "/users", to: "users#index"
   get "/", to: "users#index"
