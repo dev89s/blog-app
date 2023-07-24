d0f66385ad8a9220a710cb01f2c5ae6b7503a1fa
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-25 01:15:08 +0300

    Add eager_loading (includes) to posts#index & posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where(author_id: params[:user_id]).order(updated_at: :desc)
+    @posts = Post.includes(:comments).where(author_id: params[:user_id]).order(updated_at: :desc)
     @user = User.where(id: params[:user_id])[0]
   end
 
   def show
-    @user = User.where(id: params[:user_id])[0]
-    @post = Post.includes(:author).where(author_id: params[:user_id], id: params[:id])[0]
-    @comments = Comment.where(post_id: params[:id])
+    @user = User.find(params[:user_id])
+    @post = Post.includes(comments: [:author]).where(id: params[:id], comments: {author_id: params[:user_id]})[0]
+    @comments = @post.comments
   end
 
   def new
