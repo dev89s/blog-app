4dfe8e5a5e84d3db19e76449dbcc9e152106ae4f
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-29 16:26:14 +0300

    Initializer for active model serializer & Rack:Cors config

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module BlogApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://127.0.0.1:3000'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      end
+    end
   end
 end
